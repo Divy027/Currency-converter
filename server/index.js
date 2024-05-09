bc3a1913cf951a6e5a16041bbf33e06138e2589b
Divy <divyprajapati027@gmail.com>
2024-05-09 23:05:45 +0530

    swagger implementation

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ const express = require("express")
 const app = express()
 const router = require("./routes");
 const cors = require("cors")
-
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express")
 require('dotenv').config();
 
 const PORT = process.env.PORT
@@ -10,6 +11,24 @@ const PORT = process.env.PORT
 app.use(cors())
 app.use(express.json())
 app.use("/api/v1", router)
+
+const options = {
+    definition: {
+        openapi : "3.0.0",
+        info : {
+            title: "Crypto-Currency converter API",
+            version: "1.0.0"
+        },
+        servers : [
+            {
+                url: 'https://currency-converter-puce-tau.vercel.app/'
+            }
+        ]
+    },
+    apis: ['./routes/index.js']
+}
+const swaggerSpec = swaggerJSDoc(options);
+app.use('/api-docs',swaggerUI.serve, swaggerUI.setup(swaggerSpec))
 
 app.listen(PORT, ()=>{
     console.log(`app is listening on ${PORT}`)
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,9 +4,68 @@ const cryptoController = require("../controllers/cryptoController");
 
 
 // Crypto Convertor Routes
-
+/**
+ * @swagger
+ * /api/v1/getList:
+ *  get:
+ *      summary: This api is used to get list of crypto currencies
+ *      description: This api is used to get list of crypto currencies
+ *      responses: 
+ *          200: 
+ *              description: succesfull list is fetched
+ *          500:
+ *              description: Error in response 
+ *  
+ */
 router.get("/getList", cryptoController.apiCryptoList);
+
+/**
+ * @swagger
+ * /api/v1/getCurr:
+ *  get:
+ *      summary: This api is used to get list of currencies
+ *      description: This api is used to get list of currencies
+ *      responses: 
+ *          200: 
+ *              description: succesfull list is fetched
+ *          500:
+ *              description: Error in response 
+ *  
+ */
 router.get("/getCurr", cryptoController.apiCurrencyList);
+
+/**
+ * @swagger
+ * /api/v1/getExchangeRate:
+ *  get:
+ *     summary: Get exchange rate
+ *     description: Retrieve the exchange rate for converting between two currencies.
+ *     parameters:
+ *       - in: query
+ *         name: amount
+ *         required: true
+ *         description: The amount to convert.
+ *         schema:
+ *           type: number
+ *       - in: query
+ *         name: source
+ *         required: true
+ *         description: The source crypto-currency ID.
+ *         schema:
+ *           type: string
+ *       - in: query
+ *         name: target
+ *         required: true
+ *         description: The target currency symbol.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       '200':
+ *         description: Exchange rate retrieved successfully.
+ *       
+ *       '500':
+ *         description: Internal server error.
+ */
 router.get("/getExchangeRate", cryptoController.apiExchangeRate)
 
 
